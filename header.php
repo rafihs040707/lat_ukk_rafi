8b23cec79a67ad2780614929a66de5a5b45ca1ce
rafi <rafihaykal040707@gmail.com>
2025-09-16 11:31:42 +0700

    16 09 2025

--- header.php
+++ header.php
@@ -48,12 +48,10 @@
                         <a class="nav-link" href="index.php">Beranda</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="profil.php" role="button">
                             Profil
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="profil.php">Profil</a></li>
                             <li><a class="dropdown-item" href="identitas.php">Identitas Sekolah </a></li>
                             <li><a class="dropdown-item" href="sejarah.php">Sejarah</a></li>
                             <li><a class="dropdown-item" href="visi&mmisi.php">Visi & Misi</a></li>
